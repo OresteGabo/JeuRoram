006b8f5ce1f6b334ec3821804c5851c3adfbb411
Oreste <muhirwa.g.oreste@gmail.com>
2021-01-24 12:12:07 +0100

    Declaration of all needed global variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+const int NMAXLIGS = 8;
+const int NMAXCOLS = 8;
+const int VMAX = 9;
+const int MIRMAX = 4;
+const int LIBRE = 0;
+const int VMIROIR1 = -1;
+const int VMIROIR2 = -2;
+const int EVERTICAL = 1;
+const int EHORIZ = 2;
+const int NJOUEURS = 3;
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
