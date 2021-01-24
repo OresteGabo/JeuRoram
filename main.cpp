0d253220e81c41b2121f7bd79349bca35087b5d4
Oreste <muhirwa.g.oreste@gmail.com>
2021-01-24 12:15:46 +0100

    Declaration of all needed types

--- main.cpp
+++ main.cpp
@@ -36,6 +36,56 @@ struct Scores {
 };
 
 
+/**
+ * Contient la direction du robot
+ */
+struct direction {
+    int diry, dirx;
+};
+
+/**
+ * Contient le resultat d'un parcours du robot
+ */
+struct RParcours {
+    int nbObjets;
+    int score;
+};
+
+/**
+ * Contient tous les parametres du jeu
+ */
+struct ParamsJeu {
+    int nbLignes;
+    int nbColonnes;
+    int nbJoueurs;
+    int nbObjets;
+    int nbMiroirs;
+    int bonus;
+};
+
+/**
+ * Contient les donnees necessaires au jeu
+ */
+struct JeuRoRam {
+    Grille GR;
+    Scores score;
+    int NbObjets;
+};
+
+/**
+ * Pour la structure "choixIA"
+ */
+struct positionEtTypeMiroir {
+    int x, y, typeMiroir;
+};
+
+/**
+ * Contient le choix de l'IA du placement des miroirs
+ */
+struct choixIA {
+    positionEtTypeMiroir tab[MIRMAX];
+};
+
 
 
 
